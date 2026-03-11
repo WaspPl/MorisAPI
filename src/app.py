c13836c110a4dd9bd12ba4a7a63dc02304d76cbb
WaspPl <sebakis04@gmail.com>
2026-03-11 16:12:51 +0100

    removed the /photo endpoint

--- src/app.py
+++ src/app.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from routes import commands,messages, photo, role_assignments, sprites, status, auth, users, roles, prompts
+from routes import commands,messages, role_assignments, sprites, status, auth, users, roles, prompts
 
 app = FastAPI()
 
 app.include_router(role_assignments.router)
 app.include_router(commands.router)
 app.include_router(messages.router)
-app.include_router(photo.router)
 app.include_router(status.router)
 app.include_router(auth.router)
 app.include_router(users.router)
